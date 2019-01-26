57766d319fd8b3a3e06cf0afc883607e4cadb61a
zqlyes <1959840503@qq.com>
2019-01-26 21:14:56 +0800

    品优购项目完成

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,6 +1,7 @@
 import wepy from 'wepy'
 
 const BASE_URL = 'https://itjustfun.cn/api/public/v1/'
+const BASE_URL_LOCAL = 'http://localhost:8888/api/public/v1/'
 function fetch(url, options = {}) {
   if (typeof url === 'string') {
     options.url = url
@@ -8,10 +9,16 @@ function fetch(url, options = {}) {
   if (typeof url === 'object') {
     options = url
   }
+  if (options.type === 'locale') {
+    options.url = BASE_URL_LOCAL + options.url
+  } else {
+    options.url = BASE_URL + options.url
+  }
   return wepy.request({
-    url: BASE_URL + options.url,
-    method: options.method || 'get',
-    data: options.data || {}
+    url: options.url,
+    method: options.method || 'GET',
+    data: options.data || {},
+    header: options.header || {}
   })
 }
 
